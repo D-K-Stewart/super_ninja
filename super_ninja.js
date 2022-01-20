27269f0af6fa0a04e1e2248a54554c754fa13e3e
D-K-Stewart <90126394+D-K-Stewart@users.noreply.github.com>
2022-01-19 16:31:20 -0800

    Initial commit

---
+++ super_ninja.js
@@ -0,0 +1,47 @@
+class Ninja{
+constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.speed = 3;
+    this.strength = 3;
+}
+
+sayName() {
+    console.log(this.name);
+    return this;
+}
+
+showStats() {
+    console.log("Name: " + this.name + ", Health: " + this.health + ", Speed: " + this.speed + ", Strength: " + this.strength);
+    return this;
+}
+
+drinkSake () {
+    this.health += 10;
+    return this;
+    }
+}
+
+class Sensei extends Ninja {
+constructor(name) {
+    super(name);
+    this.health = 200;
+    this.speed = 10;
+    this.strength = 10;
+    this.wisdom = 10;
+}
+speakWisdom(){
+    const drinksak = super.drinkSake();
+    drinksak;
+    console.log("Wisdom: " + this.wisdom + ", 'What one programmer can do in one month, two programmers can do in two months.'")
+    return this
+}
+
+
+
+// example output
+// const superSensei = new Sensei("Master Splinter");
+// superSensei.speakWisdom();
+// // -> "What one programmer can do in one month, two programmers can do in two months."
+// superSensei.showStatscopy();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
